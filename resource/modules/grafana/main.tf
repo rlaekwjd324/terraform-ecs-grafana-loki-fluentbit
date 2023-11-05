7712c11be051546bf38545927434dd3ba6362bc2
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-11-05 19:05:57 +0900

    grafana alert 설정 - 대시보드, 데이터소스까지 완료

--- resource/modules/grafana/main.tf
+++ resource/modules/grafana/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "grafana" {
-  url = "13.3.0.1:3000"
+  url = "http://3.37.88.24:3000/"
   auth = "admin:admin"
 }
 
@@ -61,10 +61,8 @@ resource "grafana_notification_policy" "my_policy" {
     }
 }
 
-resource "grafana_folder" "rule_folder" {
-  title = "My Alert Rule Folder"
-}
-
+# 여기부터 다시 시작
+# 수동으로 alert rule 만들어서 그 내용과 비교하면서 값 채워나가기
 resource "grafana_rule_group" "my_alert_rule" {
   name             = "My Rule Group"
   folder_uid       = grafana_folder.rule_folder.uid
@@ -77,11 +75,9 @@ resource "grafana_rule_group" "my_alert_rule" {
     no_data_state  = "NoData"
     exec_err_state = "Alerting"
     annotations = {
-      "a" = "b"
-      "c" = "d"
     }
     labels = {
-      "type" = "error"
+      "type" = "ERROR"
     }
     is_paused = false
     data {
@@ -147,9 +143,66 @@ EOT
   }
 }
 
+resource "grafana_folder" "rule_folder" {
+  title = "My Alert Rule Folder"
+}
+
 resource "grafana_data_source" "loki" {
-  name                   = "Loki"  # 데이터 소스의 이름
+  name                   = "Loki1"  # 데이터 소스의 이름
   type                   = "loki"              # 데이터 소스 유형 (MySQL의 경우 "mysql")
   access_mode            = "proxy"             # 데이터 소스에 액세스하는 방법
   url                    = "http://loki:3100" # MySQL 데이터베이스의 주소
+}
+
+resource "grafana_dashboard" "loki" {
+  folder = grafana_folder.data_source_dashboards.id
+  config_json = jsonencode({
+    "editable": true,
+    "panels": [
+      {
+        "type": "timeseries",
+        "title": "My Panel",
+        "datasource": "${grafana_data_source.loki.name}",
+        "targets": [
+          {
+            "refId": "A",
+            "queryType": "range",
+            "expr": "rate({ecs_task_definition=\"dev-terraform-springboot:14\"}[$__interval])",
+            "alias": "My Metric"
+          }
+        ]
+      }
+    ],
+    "time": {
+      "from": "now-6h",
+      "to": "now"
+    },
+    "title": "My Dashboard test"  # 대시보드의 이름은 여기에 정의합니다.
+  })
+}
+
+resource "grafana_folder" "data_source_dashboards" {
+  title = "test folder data_source_dashboards"
+}
+
+resource "grafana_dashboard" "loki_dashboard" {
+  folder = grafana_folder.data_source_dashboards.id
+  config_json = jsonencode({
+    id            = 23456
+    title         = "data_source_dashboards 1"
+    tags          = ["data_source_dashboards"]
+    timezone      = "browser"
+    schemaVersion = 16
+  })
+}
+
+data "grafana_dashboard" "from_id" {
+  dashboard_id = grafana_dashboard.loki.dashboard_id
+}
+
+data "grafana_dashboard" "from_uid" {
+  depends_on = [
+    grafana_dashboard.loki
+  ]
+  uid = "test-ds-dashboard-uid"
 }
